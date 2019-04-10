3c54066e3646b9d43782185aef2897fba023aa72
Liangxiayo <790680635@qq.comYAO921117LXYssh-keygen -t rsa -C 790680635@qq.com>
2019-04-10 22:24:38 +0800

    mac

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ export default {
   name: "App",
   components: {
     //Index
+  },
+  created(){
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import mui from 'common/js/mui.min.js'
 
 Vue.config.productionTip = false
 
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
